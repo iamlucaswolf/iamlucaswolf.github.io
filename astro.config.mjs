0fb7a236c2da1c18682c6fd8d5e1b2f14fb0a8d5
Lucas Wolf <iamlucaswolf@gmail.com>
2023-10-27 19:50:14 +0000

    chore: Add GitHub Actions workflow for deployment to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
+  site: 'https://iamlucaswolf.github.io',
   integrations: [tailwind()]
 });
