1e41d91c4c4ec7cccb1ed00f2e8ed224677360c6
Lucas Wolf <iamlucaswolf@gmail.com>
2025-08-17 12:46:47 +0200

    feat: integrate TailwindCSS, add basic styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
-    site: 'https://iamlucaswolf.com',
+  site: 'https://iamlucaswolf.com',
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
